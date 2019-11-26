542c9c8f8424cea8b7b5e701c81bc07e3f78aca5
Nico Dorn <14349333+nicodorn@users.noreply.github.com>
2019-11-26 11:37:39 +0100

    xml.js: Kopieren von Belegschnitt

--- js/app/xml.js
+++ js/app/xml.js
@@ -3,7 +3,157 @@
 let xml = {
 	// markierten Belegschnitt in die Zwischenablage kopieren
 	schnitt () {
-// 		hier liegt der Text: popup.textauswahl.html
+		let data = popup.referenz,
+			ns = "http://www.w3.org/1999/xhtml";
+		// <Beleg>
+		let parser = new DOMParser(),
+			schnitt = parser.parseFromString("<Beleg></Beleg>", "text/xml");
+		// Attribut: xml:id
+		schnitt.firstChild.setAttribute("xml:id", xml.belegId());
+		// Attribut: Fundort
+		// (wird schon hier benötigt, um die Absätze in DWDS-Belgen in Leerzeichen zu verwandeln)
+		let fundort = "";
+		if (/^DTA/i.test(data.kr) ||
+				/deutschestextarchiv\.de\//.test(data.qu)) {
+			fundort = "dta";
+		} else if (/^DWDS/i.test(data.kr)) {
+			fundort = "dwds";
+		} else if (/^Google\s?Books$/i.test(data.kr) ||
+			/books\.google\.[a-z]+\//.test(data.qu)) {
+			fundort = "gbooks";
+		} else if (/^(DeReKo|IDS-Archiv)/i.test(data.kr)) {
+			fundort = "IDS-Archiv";
+		} else if (/https?:\/\/|www\.[a-z-]+\.[a-z]+/.test(data.qu)) {
+			fundort = "online";
+		} else {
+			fundort = "Bibliothek";
+		}
+		// <Belegtext>
+		let cont = document.createElement("div");
+		cont.innerHTML = helfer.typographie(popup.textauswahl.xml);
+		let text = "",
+			knoten = cont.childNodes;
+		for (let i = 0, len = knoten.length; i < len; i++) {
+			if (i > 0) {
+				if (fundort === "dwds") {
+					// Absätze wurden in DWDS-Belegen intern getilgt; die erscheinen online nur,
+					// um den Kontext besser zu erkennen.
+					text += " ";
+				} else {
+					text += "<lb/>";
+				}
+			}
+			getText(knoten[i]);
+		}
+		// Belegtext aufbereiten
+		//   - Trennzeichen automatisch ersetzen
+		text = text.replace(/<Autorenzusatz>\[¬\]<\/Autorenzusatz>([A-ZÄÖÜ])/g, (m, p1) => `-${p1}`);
+		text = text.replace(/<Autorenzusatz>\[¬\]<\/Autorenzusatz>/g, "");
+		// Belegtext einhängen
+		let belegtext = parser.parseFromString(`<Belegtext>${text}</Belegtext>`, "text/xml");
+		schnitt.firstChild.appendChild(belegtext.firstChild);
+		// Elemente und Text extrahieren
+		//   n = Knoten
+		//     (Knoten, der geparst werden soll)
+		function getText (n) {
+			if (n.nodeType === 1) {
+				for (let c of n.childNodes) {
+					let close = "";
+					if (c.nodeType === 1 &&
+							c.nodeName === "MARK") {
+						if (/wortFarbe[0-9]+/.test(c.getAttribute("class"))) {
+							// das ist für Kollokationen gedacht
+							// TODO da muss man wohl einen speziellen Tag verwenden
+							text += `<erwaehntes_Zeichen Sprache="dt">`;
+							close = "</erwaehntes_Zeichen>";
+						} else {
+							text += "<Stichwort>";
+							close = "</Stichwort>";
+						}
+					} else if (c.nodeType === 1 &&
+							!c.classList.contains("annotierung-wort")) {
+						// visuelle Textauszeichnung
+						// TODO Natur der Auszeichnung markieren (fett, kursiv, unterstrichen usw.)
+						text += "<Hervorhebung>";
+						close = "</Hervorhebung>";
+					}
+					getText(c);
+					if (close) {
+						text += close;
+					}
+				}
+			} else if (n.nodeType === 3) {
+				let textEsc = xml.escape(n.nodeValue);
+				textEsc = textEsc.replace(/&/g, "&amp;"); // sonst macht der Parser die &quot; usw. wieder weg
+				text += textEsc.replace(/\[.+?\]/g, m => `<Autorenzusatz>${m}</Autorenzusatz>`);
+			}
+		}
+		// <Fundstelle>
+		let fundstelle = document.createElementNS(ns, "Fundstelle");
+		schnitt.firstChild.appendChild(fundstelle);
+		fundstelle.setAttribute("Fundort", fundort);
+		// <Datum>
+		let da = xml.datum(data.da);
+		if (da) {
+			let datum = document.createElementNS(ns, "Datum");
+			fundstelle.appendChild(datum);
+			datum.appendChild(document.createTextNode(da));
+		}
+		// <Autor>
+		let au = helfer.textTrim(data.au, true);
+		if (au) {
+			let autor = document.createElementNS(ns, "Autor");
+			fundstelle.appendChild(autor);
+			autor.appendChild( document.createTextNode( xml.escape( helfer.typographie(au) ) ) );
+		}
+		// <URL>
+		let href = data.qu.match(/https?:[^\s]+|www\.[^\s]+/);
+		if (href) {
+			href[0] = href[0].replace(/(&gt;|[.:,;!?)\]}>]+)$/, "");
+			if (!/^https?:/.test(href[0])) {
+				href[0] = `https://${href[0]}`;
+			}
+			let url = document.createElementNS(ns, "URL");
+			fundstelle.appendChild(url);
+			url.appendChild( document.createTextNode( xml.escape(href[0]) ) );
+			// <Aufrufdatum>
+			let reg = new RegExp(helfer.escapeRegExp(href[0])),
+				zugriff = xml.datum(data.qu.split(reg)[1]);
+			if (zugriff) {
+				let aufrufdatum = document.createElementNS(ns, "Aufrufdatum");
+				fundstelle.appendChild(aufrufdatum);
+				aufrufdatum.appendChild(document.createTextNode(zugriff));
+			}
+		}
+		// <unstrukturiert>
+		let qu = data.qu;
+		if (href) {
+			let reg = new RegExp(helfer.escapeRegExp(href[0]));
+			qu = qu.split(reg)[0];
+		}
+		qu = helfer.textTrim(qu, true);
+		let unstrukturiert = document.createElementNS(ns, "unstrukturiert");
+		fundstelle.appendChild(unstrukturiert);
+		unstrukturiert.appendChild( document.createTextNode( xml.escape( helfer.typographie(qu) ) ) );
+		// Text in String umwandeln und aufbereiten
+		let XMLString = new XMLSerializer().serializeToString(schnitt);
+		XMLString = XMLString.replace(/\sxmlns="http:\/\/www\.w3\.org\/1999\/xhtml"/g, "");
+		let zeichen = new Map([
+			["&amp;lt;", "&lt;"],
+			["&amp;gt;", "&gt;"],
+			["&amp;amp;", "&amp;"],
+			["&amp;quot;", "&quot;"],
+			["&amp;apos;", "&apos;"],
+		]);
+		for (let [k, v] of zeichen) {
+			let reg = new RegExp(k, "g");
+			XMLString = XMLString.replace(reg, v);
+		}
+		// Text kopieren
+		helfer.toClipboard({
+			text: XMLString,
+		});
+		// Animation
 		helfer.animation("zwischenablage");
 	},
 	// Referenztag des Belegs in die Zwischenablage kopieren
@@ -23,23 +173,75 @@ let xml = {
 			autor = "n-n";
 		} else {
 			autor = autor.split(",")[0];
-			autor = autor.replace(/[,;.:'"+*!?(){}[\]]/g, "");
+			autor = autor.replace(/[;.:'"„“”‚‘»«›‹+*!?(){}[\]]/g, "");
 			autor = helfer.textTrim(autor, true);
 			autor = autor.toLowerCase();
 			autor = autor.replace(/\s/g, "-");
 		}
 		// Jahr
 		let jahr = "",
-			datum = data.da.match(/[0-9]{4}/);
+			datum = xml.datum(data.da).match(/[0-9]{4}/);
 		if (datum) {
 			jahr = datum[0];
-		} else {
-			let jh = data.da.match(/([0-9]{2})\.\sJh\./);
-			if (jh){ // das Datumsfeld könnte in Karteikarten noch nicht ausgefüllt sein
-				jahr = `${parseInt(jh[1], 10) - 1}00`;
-			}
 		}
 		// ID zurückgeben
 		return `${autor}-${jahr}`;
+	},
+	// Datum extrahieren
+	//   text = String
+	//     (Text, aus dem heraus das Datum extrahiert werden soll)
+	datum (text) {
+		let formate = [
+			/(?<tag>[0-9]{1,2})\.\s*(?<monat>[0-9]{1,2})\.\s*(?<jahr>[0-9]{4})/,
+			/(?<jahr>[0-9]{4})-(?<monat>[0-9]{2})-(?<tag>[0-9]{2})/,
+			/(?<jahr>[0-9]{4})/,
+			/(?<jahrhundert>[0-9]{2})\.\sJh\./,
+		];
+		let jahr = "",
+			monat = 0,
+			tag = 0;
+		for (let reg of formate) {
+			let m = text.match(reg);
+			if (m) {
+				if (m.groups.jahrhundert) {
+					jahr = `${parseInt(m.groups.jahrhundert, 10) - 1}00`; // sehr unschön
+				} else {
+					// steht vor diesem Datum ein anderes Datum, das Vorrang hat?
+					let before = text.substring(0, m.index);
+					if (/[0-9]{4}|[0-9]{2}\.\sJh\./.test(before)) {
+						continue;
+					}
+					// dieses Datum wird ausgewertet
+					jahr = m.groups.jahr;
+					monat = parseInt(m.groups.monat, 10);
+					tag = parseInt(m.groups.tag, 10);
+				}
+				break;
+			}
+		}
+		if (jahr) {
+			if (!monat || ! tag) {
+				return jahr;
+			}
+			return `${tag < 10 ? "0" : ""}${tag}.${monat < 10 ? "0" : ""}${monat}.${jahr}`;
+		}
+		return "";
+	},
+	// geschützte Zeichen escapen
+	//   text = String
+	//     (String, der escaped werden soll)
+	escape (text) {
+		let zeichen = new Map([
+			["<", "&lt;"],
+			[">", "&gt;"],
+			["&", "&amp;"],
+			['"', "&quot;"],
+			["'", "&apos;"],
+		]);
+		for (let [k, v] of zeichen) {
+			let reg = new RegExp(k, "g");
+			text = text.replace(reg, v);
+		}
+		return text;
 	},
 };
