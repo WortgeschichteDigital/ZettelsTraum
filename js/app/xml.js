f9175ed401b6587d4a6a9b11519617b850af3390
Nico Dorn <14349333+nicodorn@users.noreply.github.com>
2021-07-03 11:01:38 +0200

    Fix: Stichwörter, die eine Markierungsfarbe erhielten, wurden beim Erstellen eines XML-Snippets fälschlicherweise mit <Stichwort> und nicht mit <Markierung> ausgezeichnet

--- js/app/xml.js
+++ js/app/xml.js
@@ -107,7 +107,7 @@ let xml = {
 			}
 			return m;
 		});
-		text = text.replace(/<\/Stichwort><Stichwort>/g, "");
+		text = text.replace(/<\/Stichwort><Stichwort>|<\/Markierung><Markierung>/g, "");
 		text = helfer.textTrim(text, true);
 		text = xml.mergeHervorhebungen({text});
 		// Belegtext einhängen
@@ -130,7 +130,9 @@ let xml = {
 						// aufnimmt, kann es zu Verschachtelungen kommen
 						const verschachtelt = c.parentNode.closest("mark");
 						if (!verschachtelt &&
-								(c.classList.contains("user") || c.classList.contains("markierung"))) {
+								(c.parentNode.classList.contains("annotierung-wort") ||
+								c.classList.contains("user") ||
+								c.classList.contains("markierung"))) {
 							text += `<Markierung>`;
 							close = "</Markierung>";
 						} else if (!verschachtelt) {
