50e885f6686f7dd3a3e7d548458369d640abc426
Nico Dorn <14349333+nicodorn@users.noreply.github.com>
2023-10-20 12:52:31 +0200

    Fix: wurde das im geöffneten Bedeutungsgerüst-Fenster sichtbare Bedeutungsgerüst im Hauptfenster gelöscht, führte dies im Bedeutungsgerüst-Fenster zu einem Fehler, sodass überhaupt kein Gerüst mehr angezeigt wurde

--- js/win/bedeutungen.js
+++ js/win/bedeutungen.js
@@ -15,16 +15,24 @@ const bedeutungen = {
 
   // Anzeige mit den gelieferten Daten aufbereiten
   aufbauen () {
+    // falls das aktuelle Bedeutungsgerüst gelöscht wurde => aktuelles Gerüst des Hauptfensters auswählen
+    if (bedeutungen.geruest && !bedeutungen.data.bd.gr[bedeutungen.geruest]) {
+      bedeutungen.geruest = bedeutungen.data.bd.gn;
+    }
+
     // Wort eintragen
     document.title = bedeutungen.data.titel;
     document.querySelector("h1").innerHTML = bedeutungen.data.wort;
+
     // ggf. Gerüstnummer zwischenspeichern
     if (!bedeutungen.geruest) {
       bedeutungen.geruest = bedeutungen.data.bd.gn;
     }
+
     // Content leeren
     const cont = document.getElementById("bd-win-cont");
     cont.replaceChildren();
+
     // Details zum Bedeutungsgerüst in die Überschrift eintragen
     let detail = "";
     if (Object.keys(bedeutungen.data.bd.gr).length > 1) {
@@ -34,8 +42,10 @@ const bedeutungen = {
       detail += `\u00A0(${bedeutungen.data.bd.gr[bedeutungen.geruest].na})`;
     }
     document.getElementById("bd-win-geruest-detail").textContent = detail;
+
     // Gerüst-Nummer in Dropdown-Feld
     document.getElementById("bd-win-gerueste").value = `Gerüst ${bedeutungen.geruest}`;
+
     // Sind überhaupt Bedeutungen vorhanden?
     const bd = bedeutungen.data.bd.gr[bedeutungen.geruest].bd;
     if (!bd.length) {
@@ -45,6 +55,7 @@ const bedeutungen = {
       cont.appendChild(p);
       return;
     }
+
     // Bedeutungen aufbauen
     const icons = [ "icon-add", "icon-remove" ];
     const table = document.createElement("table");
@@ -52,11 +63,13 @@ const bedeutungen = {
     for (let i = 0, len = bd.length; i < len; i++) {
       const tr = document.createElement("tr");
       table.appendChild(tr);
+
       // Zellen erzeugen
       for (let j = 0; j < 3; j++) {
         const td = document.createElement("td");
         tr.appendChild(td);
       }
+
       // Icons erzeugen
       for (let j = 0; j < 2; j++) {
         const a = document.createElement("a");
@@ -74,6 +87,7 @@ const bedeutungen = {
           bedeutungen.umtragen(a);
         }
       }
+
       // Schachteln erzeugen
       const frag = document.createDocumentFragment();
       let schachtel = frag;
@@ -83,6 +97,7 @@ const bedeutungen = {
         div.classList.add("bd-win-baum");
         schachtel = div;
       }
+
       // Absatz mit Zählung und Bedeutung
       const p = document.createElement("p");
       schachtel.appendChild(p);
@@ -90,15 +105,18 @@ const bedeutungen = {
       p.dataset.eintragen = "true";
       p.title = "Bedeutung eintragen";
       bedeutungen.umtragen(p);
+
       // Zählung
       const b = document.createElement("b");
       p.appendChild(b);
       b.classList.add("zaehlung");
       b.textContent = bd[i].za;
+
       // Bedeutung
       const span = document.createElement("span");
       p.appendChild(span);
       span.innerHTML = bd[i].bd[bd[i].bd.length - 1];
+
       // Fragment einhängen
       tr.lastChild.appendChild(frag);
     }
